132dc95e6b62bc1beaf974f058f260f9f8121a76
mbuenoferrer <mbuenoferrer@gmail.com>
2017-02-26 20:00:04 +0100

    Update the MainActivity when load and changes preferences

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -16,6 +16,12 @@
 package com.example.android.sunshine.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+
+import com.example.android.sunshine.R;
+
+import java.util.Objects;
 
 public class SunshinePreferences {
 
@@ -88,6 +94,14 @@ public class SunshinePreferences {
      */
     public static String getPreferredWeatherLocation(Context context) {
         // TODO (1) Return the user's preferred location
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if(sharedPreferences.contains(context.getString(R.string.pref_location_key))) {
+            return sharedPreferences.getString(
+                    context.getString(R.string.pref_location_key),
+                    context.getString((R.string.pref_location_default)));
+        }
+
         /** This will be implemented in a future lesson **/
         return getDefaultWeatherLocation();
     }
@@ -101,8 +115,24 @@ public class SunshinePreferences {
      */
     public static boolean isMetric(Context context) {
         // TODO (2) Return true if the user's preference for units is metric, false otherwise
-        /** This will be implemented in a future lesson **/
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if(sharedPreferences.contains(context.getString(R.string.pref_units_key))) {
+            String units = sharedPreferences.getString(
+                    context.getString(R.string.pref_units_key),
+                    context.getString((R.string.pref_units_metric)));
+
+            if(units.equals(context.getString((R.string.pref_units_metric)))){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
         return true;
+
+        /** This will be implemented in a future lesson **/
     }
 
     /**
